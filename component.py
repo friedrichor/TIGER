db98708d2e10cfa14c44490196b91932f0f92c5f
friedrichor <kongfanheng426@163.com>
2023-07-31 23:57:58 +0800

    Update component.py

--- component.py
+++ component.py
@@ -3,7 +3,7 @@ import gradio as gr
 
 
 
-def decoding_setting(stratey: str, num_beams: gr.Slider, p_value: gr.Slider, k_value: gr.Slider):
+def decoding_setting(stratey: str):
     print(stratey)
     if stratey == "Beam":
         return gr.Slider.update(visible=True), gr.Slider.update(visible=False), gr.Slider.update(visible=False)
